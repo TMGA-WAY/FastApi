36c44e44b33020fd6a246438f360ceab38d45bc8
Mitra-babu <dipankar9612@gmail.com>
2023-05-06 22:05:31 +0530

    Query parameters added

--- rounter/blog_post.py
+++ rounter/blog_post.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Body
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 router = APIRouter(
     prefix="/blog",
@@ -27,8 +27,13 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
 def create_comment(blog: BlogModel, id: int,
                    comment_id: int = Query(None,
                                            title="ID of the comment",
-                                           description="Some description")):
+                                           description="Some description"),
+                   content: str = Body(Ellipsis, min_length=10),
+                   v: Optional[List[str]] = Query(None)):
     return {
         'id': id,
         'blog': blog,
-        'comment_id': comment_id}
+        'comment_id': comment_id,
+        'content': content,
+        'v': v
+    }
