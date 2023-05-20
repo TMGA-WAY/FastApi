4d53e3331b6646ad0e61cbdd88c4c8695156a520
Mitra-babu <dipankar9612@gmail.com>
2023-05-20 13:03:30 +0530

    update is added

--- db/db_user.py
+++ db/db_user.py
@@ -14,3 +14,22 @@ def create_user(db: Session, request: user_base):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+def get_all(db: Session):
+    return db.query(db_user).all()
+
+
+def get_user(db: Session, id_: int):
+    return db.query(db_user).filter(db_user.id == id_).first()
+
+
+def update_user(db: Session, id_: int, request: user_base):
+    user = db.query(db_user).filter(db_user.id == id_)
+    user.update({
+        db_user.user_name: request.user_name,
+        db_user.email: request.email,
+        db_user.password: hash_user.bcrypt(request.password)
+    })
+    db.commit()
+    return 'ok'
--- router/user.py
+++ router/user.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from typing import List
 from schema import user_base
 from sqlalchemy.orm import Session
 from db.databse import get_db
@@ -15,3 +16,13 @@ router = APIRouter(
 @router.post('/', response_model=user_display)
 def create_user(request: user_base, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
+
+
+@router.get('/', response_model=List[user_display])
+def get_all_user(db: Session = Depends(get_db)):
+    return db_user.get_all(db)
+
+
+@router.get("/{id_}", response_model=user_display)
+def get_user(id_: int, db: Session = Depends(get_db)):
+    return db_user.get_user(db=db, id_=id_)
