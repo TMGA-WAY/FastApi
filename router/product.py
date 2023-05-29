577f377b3e5ad9374d01543799d1e85e903ef02f
Mitra-babu <dipankar9612@gmail.com>
2023-05-29 22:40:13 +0530

    product added

--- router/product.py
+++ router/product.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from fastapi.responses import Response, HTMLResponse
+from fastapi.responses import Response, HTMLResponse, PlainTextResponse
 
 router = APIRouter(
     prefix="/product",
@@ -9,24 +9,28 @@ router = APIRouter(
 products = ['watch', 'camera', 'phone']
 
 
-@router.get("/all", response_model=Response)
+@router.get("/all")
 def get_all_product() -> Response:
     data = " ".join(products)
     return Response(content=data, media_type='text/plain')
 
 
 @router.get('/{id_}')
-def get_product(id_: int) -> HTMLResponse:
-    product = products[id_]
-    out = f"""
-    <head>
-        <style>
-            -product {{width:500px; height: 30px;
-             border: 2px inset green;
-             background-color : lightblue;
-             text-align: center}};
-        </style>
-    </head>
-    <div class="product">{product} </div>
-    """
-    return HTMLResponse(content=out, media_type="text/html")
+def get_product(id_: int):
+    if id_ > len(products):
+        out = "Product not available"
+        return PlainTextResponse(content=out, media_type='text/plain')
+    else:
+        product = products[id_]
+        out = f"""
+        <head>
+            <style>
+                -product {{width:500px; height: 30px;
+                 border: 2px inset green;
+                 background-color : lightblue;
+                 text-align: center}};
+            </style>
+        </head>
+        <div class="product">{product} </div>
+        """
+        return HTMLResponse(content=out, media_type="text/html")
