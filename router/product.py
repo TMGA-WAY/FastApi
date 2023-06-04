c547f02c915a7a630521496a80dfd8a485a1caef
Mitra-babu <dipankar9612@gmail.com>
2023-06-04 23:05:50 +0530

    header added

--- router/product.py
+++ router/product.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
 from fastapi.responses import Response, HTMLResponse, PlainTextResponse
+from typing import Optional
 
 router = APIRouter(
     prefix="/product",
@@ -13,6 +14,13 @@ products = ['watch', 'camera', 'phone']
 def get_all_product() -> Response:
     data = " ".join(products)
     return Response(content=data, media_type='text/plain')
+
+
+@router.get("/with_header")
+def get_products(response: Response,
+                 custom_header: Optional[str] = Header(None)):
+    response.headers['custome_response_header'] = " and ".join(custom_header)
+    return products
 
 
 @router.get('/{id_}')
